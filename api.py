0bdf7f90784e6a793cd78f7759ed62085a811b9c
mrserjo <mrserjo@mail.ru>
2022-04-18 09:02:39 +0300

    get_video added

--- api.py
+++ api.py
@@ -1,18 +1,19 @@
 import shutil
 from typing import List
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form
 
-from schemas import UploadVideo
+from schemas import UploadVideo, User, GetVideo
 
 video_router = APIRouter()
 
 
 @video_router.post("/")
-async def root(file: UploadFile = File(...)):
+async def root(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+    info = UploadVideo(title=title, description=description)
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    return {"file_name": file.filename}
+    return {"file_name": file.filename, "info": info}
 
 
 @video_router.post("/img")
@@ -25,13 +26,8 @@ async def upload_image(files: List[UploadFile] = File(...)):
     return {"file_name": "Good"}
 
 
-@video_router.post("/info")
-async def info_set(info: UploadVideo):
-    return info
-
-
-@video_router.get("/info")
-async def info_get():
-    title = 'Test'
-    desc = 'Description'
-    return UploadVideo(title=title, description=desc)
+@video_router.get("/video")
+async def get_video():
+    user = User(**{'id': 25, 'name': 'Serj'})
+    video = UploadVideo(**{'title': 'Test', 'description': 'Description'})
+    return GetVideo(user=user, video=video)
--- schemas.py
+++ schemas.py
@@ -3,7 +3,16 @@ from typing import List
 from pydantic import BaseModel
 
 
+class User(BaseModel):
+    id: int
+    name: str
+
+
 class UploadVideo(BaseModel):
     title: str
     description: str
-    tags: List[str] = None
+
+
+class GetVideo(BaseModel):
+    user: User
+    video: UploadVideo
